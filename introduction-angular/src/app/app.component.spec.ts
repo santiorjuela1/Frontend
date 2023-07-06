83af0260fac59115ac013faf7884c6318938d4da
Santiago Orjuela <santiago.orjuela@utp.edu.co>
2023-07-06 12:33:16 -0500

    Dia 2

--- introduction-angular/src/app/app.component.spec.ts
+++ introduction-angular/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'introduction-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('introduction-angular');
+    //expect(app.title).toEqual('introduction-angular');
   });
 
   it('should render title', () => {
--- introduction-angular/src/app/app.component.ts
+++ introduction-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Libro } from 'src/models/Libro';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'introduction-angular y spring boot';
+  constructor(){
+    let libro: Libro = new Libro();
+
+    libro.autor = 'Diego';
+    libro.idLibro = 1;
+    libro.titulo = 'El libro de diego';
+
+    let libro2: Libro = new Libro();
+    libro2.autor = 'Diego2';
+    libro2.idLibro = 2;
+    libro2.titulo = 'El libro de diego2';
+
+    this.listaLibros.push(libro);
+    this.listaLibros.push(libro2);
+    console.log(this.listaLibros);
+
+  }
+
+  public listaLibros: Libro[] = [];
+
+  public nombre : string = 'Santiago';
+
+  public numero : number = 1;
+
+  public bool : boolean = true;
+
+
+  public nombres: string[] = ['pedro', 'juan', 'lucas'];
+
+  public nombres2 : string[] = ['matias', 'felipe', 'santiago'];
+
+  public imagenUrl = 'https://picsum.photos/200/300';
+
+
+  public activedButton: boolean = false;
+
+  public desactivarBoton(bandera : boolean): void{
+    this.activedButton = bandera;
+  }
+
+  public addFirstElementArray(name : string): void{
+    this.nombres.unshift(name);
+  }
+
+  public addLastElementArray(name :  string): void{
+    this.nombres.push(name);
+  }
+
+  public concat(list: string[]): string[]{
+    return this.nombres.concat(list);
+  }
+
+
 }
